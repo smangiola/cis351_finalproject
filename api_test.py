23789e822e9bf82ff9926f01669731e75cf29d35
smangiola <143442895+smangiola@users.noreply.github.com>
2025-04-04 15:38:59 -0400

    Add files via upload

---
+++ api_test.py
@@ -0,0 +1,6 @@
+import requests #library needed to interact with the API
+import json #library needed to convert the data into a JSON format
+api_key = 'c4d69dd993618ac74a49f967895e4a00'
+base_url = f'https://superheroapi.com/api/{api_key}'
+
+
